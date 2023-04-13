19788328521fd5140d935e812fd3bbe43f5005b3
fadoua <arfouyfadoua@gmail.com>
2023-04-13 08:08:13 -0700

    task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,42 +2,37 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the strings in av
- * @ac: the number of strings
- * @av: an array of strings
- *
- * Return: a pointer to a new string, or NULL if it fails
- */
+* argstostr - concatenates all the strings in av
+* @ac: the number of strings
+* @av: an array of strings
+* Return: a pointer to a new string, or NULL if it fails
+*/
 char *argstostr(int ac, char **av)
 {
-        int i, j, k = 0, len = 0;
-        char *str;
+int i, j, k = 0, len = 0;
+char *str;
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
-
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                        len++;
-        }
-
-        len += ac;
-        str = malloc(sizeof(char) * (len + 1));
-        if (str == NULL)
-                return (NULL);
-
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                {
-                        str[k] = av[i][j];
-                        k++;
-                }
-                str[k] = '\n';
-                k++;
-        }
-        str[k] = '\0';
-
-        return (str);
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+len++;
+}
+len += ac;
+str = malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+str[k] = av[i][j];
+{
+}
+str[k] = '\n';
+k++;
+}
+str[k] = '\0';
+return (str);
 }

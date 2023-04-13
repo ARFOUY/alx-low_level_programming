419379d91add326e6d07cdf344cbbeac3f3a91fb
fadoua <arfouyfadoua@gmail.com>
2023-04-13 10:03:32 -0700

    task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,38 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
-* argstostr - concatenates all the strings in av
-* @ac: the number of strings
-* @av: an array of strings
-* Return: a pointer to a new string, or NULL if it fails
-*/
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
 char *argstostr(int ac, char **av)
 {
-int i, j, k = 0, len = 0;
-char *str;
+	int i, n, r = 0, l = 0;
+	char *str;
 
-if (ac == 0 || av == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-len++;
-}
-len += ac;
-str = malloc(sizeof(char) * (len + 1));
-if (str == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-str[k] = av[i][j];
-{
-}
-str[k] = '\n';
-k++;
-}
-str[k] = '\0';
-return (str);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
 }

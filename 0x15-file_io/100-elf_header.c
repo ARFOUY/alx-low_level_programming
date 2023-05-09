16d1a4601f0194664bfa107da9fbe7929d8248bb
fadoua <arfouyfadoua@gmail.com>
2023-05-09 16:51:58 -0700

    task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,130 +2,89 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <elf.h>
-#include <string.h>
 
-#define BUF_SIZE 128
+#define BUF_SIZE 64
 
-void print_error(char *message)
-{
-    fprintf(stderr, "Error: %s\n", message);
-    exit(98);
-}
-
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
-        print_error("Usage: elf_header elf_filename");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        exit(98);
     }
 
-    char *filename = argv[1];
-    int fd = open(filename, O_RDONLY);
-
-    if (fd < 0)
-    {
-        print_error("Cannot open file");
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        fprintf(stderr, "Error: could not open file %s\n", argv[1]);
+        exit(98);
     }
 
-    // read the ELF header
-    Elf64_Ehdr ehdr;
-    ssize_t n = read(fd, &ehdr, sizeof(ehdr));
-
-    if (n < 0)
-    {
-        print_error("Cannot read file");
+    char buf[BUF_SIZE];
+    if (read(fd, buf, BUF_SIZE) != BUF_SIZE) {
+        fprintf(stderr, "Error: could not read ELF header from file %s\n", argv[1]);
+        exit(98);
     }
 
-    if (n != sizeof(ehdr) || memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0)
-    {
-        print_error("Not an ELF file");
+    // Check if it's an ELF file
+    if (buf[0] != 0x7f || buf[1] != 'E' || buf[2] != 'L' || buf[3] != 'F') {
+        fprintf(stderr, "Error: file %s is not an ELF file\n", argv[1]);
+        exit(98);
     }
 
-    // print the information
-    printf("Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++)
-    {
-        printf("%02x ", ehdr.e_ident[i]);
+    // Extract required information from the ELF header
+    unsigned char elf_class = buf[4];
+    unsigned char elf_data = buf[5];
+    unsigned char elf_version = buf[6];
+    unsigned char elf_osabi = buf[7];
+    unsigned char elf_abiversion = buf[8];
+    unsigned short elf_type = *(unsigned short *)(buf + 16);
+    unsigned long long elf_entry = *(unsigned long long *)(buf + 24);
+
+    // Display the extracted information
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (int i = 0; i < 16; i++) {
+        printf("%02x ", buf[i]);
     }
     printf("\n");
-
-    printf("Class:                             ");
-    switch (ehdr.e_ident[EI_CLASS])
-    {
-        case ELFCLASS32:
+    printf("  Class:                             ");
+    switch (elf_class) {
+        case 1:
             printf("ELF32\n");
             break;
-        case ELFCLASS64:
+        case 2:
             printf("ELF64\n");
             break;
         default:
-            printf("Invalid\n");
-            break;
+            printf("Invalid class\n");
+            exit(98);
     }
-
-    printf("Data:                              ");
-    switch (ehdr.e_ident[EI_DATA])
-    {
-        case ELFDATA2LSB:
+    printf("  Data:                              ");
+    switch (elf_data) {
+        case 1:
             printf("2's complement, little endian\n");
             break;
-        case ELFDATA2MSB:
+        case 2:
             printf("2's complement, big endian\n");
             break;
         default:
-            printf("Invalid\n");
+            printf("Invalid data format\n");
+            exit(98);
+    }
+    printf("  Version:                           %d (current)\n", elf_version);
+    printf("  OS/ABI:                            ");
+    switch (elf_osabi) {
+        case 0:
+            printf("UNIX - System V\n");
+            break;
+        case 3:
+            printf("UNIX - GNU\n");
+            break;
+        case 6:
+            printf("UNIX - Solaris\n");
+            break;
+        default:
+            printf("Unknown\n");
             break;
     }
-
-    printf("Version:                           %d (current)\n", ehdr.e_ident[EI_VERSION]);
-
-    printf("OS/ABI:                            ");
-    switch (ehdr.e_ident[EI_OSABI])
-    {
-        case ELFOSABI_SYSV:
-            printf("UNIX System V ABI\n");
-            break;
-        case ELFOSABI_HPUX:
-            printf("HP-UX ABI\n");
-            break;
-        case ELFOSABI_NETBSD:
-            printf("NetBSD ABI\n");
-            break;
-        case ELFOSABI_LINUX:
-printf("Linux ABI\n");
-break;
-case ELFOSABI_SOLARIS:
-printf("Solaris ABI\n");
-break;
-case ELFOSABI_IRIX:
-printf("IRIX ABI\n");
-break;
-case ELFOSABI_FREEBSD:
-printf("FreeBSD ABI\n");
-break;
-case ELFOSABI_TRU64:
-printf("TRU64 UNIX ABI\n");
-break;
-case ELFOSABI_ARM:
-printf("ARM architecture ABI\n");
-break;
-case ELFOSABI_STANDALONE:
-printf("Standalone (embedded) ABI\n");
-break;
-default:
-printf("Other\n");
-break;
-}
-
-printf("ABI Version:                       %d\n", ehdr.e_ident[EI_ABIVERSION]);
-
-printf("Type:                              ");
-switch (ehdr.e_type)
-{
-case ET_NONE:
-printf("NONE (No file type)\n");
-break;
-case ET_REL:
-printf("REL (Relocatable
+printf("  ABI Version:                       %d\n", elf_abiversion);
+ printf(" 
 
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ *
+ * @filename: The name of the file to create.
+ * @text_content: The text to write to the file.
+ *
+ * Return: On success, 1. On failure, -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (len > 0)
+	{
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
